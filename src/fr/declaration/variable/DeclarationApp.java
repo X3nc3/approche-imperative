07182954f28c1dcd82c68f90cfea543e6f7cab5e
Maxence <maxence.ogier@gmail.com>
2024-10-02 14:52:32 +0200

    second commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,21 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main (String args[]) {
+        byte var1 = 100;
+        short var2 = 30000;
+        int var3 = 5000000;
+        long var4 = 1000000000L;
+        float var5 = 1.3F;
+        double var6 = 1.4456d;
+        char var7 = 'm';
+        boolean var8 = true;
+        String var9 = "Maxence";
+
+        System.out.println(var9);
+
+        String randomString = "Voici le résultat d'un calcul : \n1+5=6";
+
+        System.out.println(randomString);
+    }
+}
