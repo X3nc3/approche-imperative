b2d2f8a57919bbe7cab9593b2d48b39c281a7ca9
Maxence <maxence.ogier@gmail.com>
2024-10-03 09:18:44 +0200

    fifth commit

--- src/fr/boucles/ExerciceBouclesEtTests.java
+++ src/fr/boucles/ExerciceBouclesEtTests.java
@@ -27,6 +27,12 @@ public class ExerciceBouclesEtTests {
             }
 
             for (int i=0; i<array.length; i++) {
+                if ( i%2 == 0) {
+                    System.out.println(array[i]);
+                }
+            }
+
+            for (int i=0; i<array.length; i++) {
                 if ( array[i]%2 != 0) {
                     System.out.println(array[i]);
                 }
